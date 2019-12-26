a8cf04c4eddea7ce0fb14a47746745b3f8ec1d64
K e n V <kenneth_valdivia@yahoo.com>
2019-12-26 02:06:52 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["john"]["paul"]["ringo"]["george"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  << ["Ken"]
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  .unshift Ken
 end
 
 def remove_element_from_end_of_array(array)
-  
+  .pop ["paul"]
 end
 
 def remove_element_from_start_of_array(array)
-  
+  .shift ["ringo"]
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  ["paul"]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  [-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+retrieve_element_from_index
 end
